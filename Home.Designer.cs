50ff42b2cf8c4aef276515465ac91449cbfae7f4
Juruel Keanu Lorenzo <keanulorenzo32@gmail.com>
2021-12-28 16:05:44 +0800

    feat(main): adjustable headers

--- Home.Designer.cs
+++ Home.Designer.cs
@@ -45,6 +45,8 @@
             this.label5 = new System.Windows.Forms.Label();
             this.cb_iBracket = new System.Windows.Forms.ComboBox();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tb_root = new System.Windows.Forms.TextBox();
             this.lv_result = new System.Windows.Forms.ListView();
             this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
@@ -52,8 +54,6 @@
             this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.tb_root = new System.Windows.Forms.TextBox();
-            this.label7 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.num_places)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.num_range)).BeginInit();
             this.groupBox1.SuspendLayout();
@@ -69,7 +69,7 @@
             this.txt_equation.Location = new System.Drawing.Point(98, 19);
             this.txt_equation.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txt_equation.Name = "txt_equation";
-            this.txt_equation.Size = new System.Drawing.Size(406, 26);
+            this.txt_equation.Size = new System.Drawing.Size(409, 26);
             this.txt_equation.TabIndex = 0;
             // 
             // label1
@@ -100,13 +100,13 @@
             this.txt_ea.Location = new System.Drawing.Point(98, 49);
             this.txt_ea.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txt_ea.Name = "txt_ea";
-            this.txt_ea.Size = new System.Drawing.Size(161, 26);
+            this.txt_ea.Size = new System.Drawing.Size(164, 26);
             this.txt_ea.TabIndex = 3;
             // 
             // btn_calculate
             // 
             this.btn_calculate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btn_calculate.Location = new System.Drawing.Point(511, 19);
+            this.btn_calculate.Location = new System.Drawing.Point(514, 19);
             this.btn_calculate.Name = "btn_calculate";
             this.btn_calculate.Size = new System.Drawing.Size(95, 56);
             this.btn_calculate.TabIndex = 4;
@@ -119,11 +119,11 @@
             this.lv_fxLimits.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.lv_fxLimits.GridLines = true;
-            this.lv_fxLimits.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
+            this.lv_fxLimits.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
             this.lv_fxLimits.HideSelection = false;
             this.lv_fxLimits.Location = new System.Drawing.Point(6, 25);
             this.lv_fxLimits.Name = "lv_fxLimits";
-            this.lv_fxLimits.Size = new System.Drawing.Size(581, 84);
+            this.lv_fxLimits.Size = new System.Drawing.Size(584, 98);
             this.lv_fxLimits.TabIndex = 5;
             this.lv_fxLimits.UseCompatibleStateImageBehavior = false;
             this.lv_fxLimits.View = System.Windows.Forms.View.Details;
@@ -131,7 +131,7 @@
             // label3
             // 
             this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.label3.Location = new System.Drawing.Point(267, 49);
+            this.label3.Location = new System.Drawing.Point(270, 49);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(60, 26);
@@ -142,7 +142,7 @@
             // num_places
             // 
             this.num_places.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.num_places.Location = new System.Drawing.Point(334, 49);
+            this.num_places.Location = new System.Drawing.Point(337, 49);
             this.num_places.Maximum = new decimal(new int[] {
             6,
             0,
@@ -161,7 +161,7 @@
             // label4
             // 
             this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.label4.Location = new System.Drawing.Point(383, 49);
+            this.label4.Location = new System.Drawing.Point(386, 49);
             this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(72, 26);
@@ -172,7 +172,7 @@
             // num_range
             // 
             this.num_range.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.num_range.Location = new System.Drawing.Point(462, 49);
+            this.num_range.Location = new System.Drawing.Point(465, 49);
             this.num_range.Maximum = new decimal(new int[] {
             20,
             0,
@@ -196,23 +196,22 @@
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox1.Location = new System.Drawing.Point(12, 97);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(594, 115);
+            this.groupBox1.Size = new System.Drawing.Size(597, 129);
             this.groupBox1.TabIndex = 10;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Function Limits";
             // 
             // groupBox2
             // 
-            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.groupBox2.Controls.Add(this.label6);
             this.groupBox2.Controls.Add(this.lv_pBrackets);
             this.groupBox2.Controls.Add(this.label5);
             this.groupBox2.Controls.Add(this.cb_iBracket);
-            this.groupBox2.Location = new System.Drawing.Point(12, 218);
+            this.groupBox2.Location = new System.Drawing.Point(12, 232);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(594, 82);
+            this.groupBox2.Size = new System.Drawing.Size(597, 75);
             this.groupBox2.TabIndex = 11;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Bracket";
@@ -234,10 +233,10 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.lv_pBrackets.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
             this.lv_pBrackets.HideSelection = false;
-            this.lv_pBrackets.Location = new System.Drawing.Point(290, 17);
+            this.lv_pBrackets.Location = new System.Drawing.Point(290, 22);
             this.lv_pBrackets.Name = "lv_pBrackets";
             this.lv_pBrackets.ShowGroups = false;
-            this.lv_pBrackets.Size = new System.Drawing.Size(298, 58);
+            this.lv_pBrackets.Size = new System.Drawing.Size(301, 44);
             this.lv_pBrackets.TabIndex = 12;
             this.lv_pBrackets.TileSize = new System.Drawing.Size(10, 10);
             this.lv_pBrackets.UseCompatibleStateImageBehavior = false;
@@ -271,12 +270,34 @@
             this.groupBox3.Controls.Add(this.label7);
             this.groupBox3.Controls.Add(this.tb_root);
             this.groupBox3.Controls.Add(this.lv_result);
-            this.groupBox3.Location = new System.Drawing.Point(12, 310);
+            this.groupBox3.Location = new System.Drawing.Point(12, 313);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(594, 339);
+            this.groupBox3.Size = new System.Drawing.Size(597, 354);
             this.groupBox3.TabIndex = 12;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Result";
+            // 
+            // label7
+            // 
+            this.label7.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label7.Location = new System.Drawing.Point(5, 316);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(441, 26);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Root of Function:";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // tb_root
+            // 
+            this.tb_root.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.tb_root.Location = new System.Drawing.Point(453, 316);
+            this.tb_root.Name = "tb_root";
+            this.tb_root.ReadOnly = true;
+            this.tb_root.Size = new System.Drawing.Size(137, 26);
+            this.tb_root.TabIndex = 1;
+            this.tb_root.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // lv_result
             // 
@@ -293,7 +314,7 @@
             this.lv_result.HideSelection = false;
             this.lv_result.Location = new System.Drawing.Point(6, 26);
             this.lv_result.Name = "lv_result";
-            this.lv_result.Size = new System.Drawing.Size(581, 269);
+            this.lv_result.Size = new System.Drawing.Size(584, 284);
             this.lv_result.TabIndex = 0;
             this.lv_result.UseCompatibleStateImageBehavior = false;
             this.lv_result.View = System.Windows.Forms.View.Details;
@@ -333,33 +354,11 @@
             this.columnHeader6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.columnHeader6.Width = 100;
             // 
-            // tb_root
-            // 
-            this.tb_root.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.tb_root.Location = new System.Drawing.Point(450, 301);
-            this.tb_root.Name = "tb_root";
-            this.tb_root.ReadOnly = true;
-            this.tb_root.Size = new System.Drawing.Size(137, 26);
-            this.tb_root.TabIndex = 1;
-            this.tb_root.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
-            // label7
-            // 
-            this.label7.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.label7.Location = new System.Drawing.Point(5, 301);
-            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(438, 26);
-            this.label7.TabIndex = 14;
-            this.label7.Text = "Root of Function:";
-            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            // 
             // Home
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(618, 661);
+            this.ClientSize = new System.Drawing.Size(621, 676);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
